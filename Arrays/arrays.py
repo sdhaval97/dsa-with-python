5e3a1d2851fce8a69e90fe087fad3bd1723b200f
sdhaval97 <sdhaval97@gmail.com>
2025-09-23 23:16:36 +0530

    learnt linear search in array

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -22,4 +22,10 @@ def accessElement(array, index):
     else:
         print(array[index])
         
-        
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+print(linear_search(my_array1, 8))

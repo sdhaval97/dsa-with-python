16d5a51a6dcaad1303caf6cb3975f77300460fd4
sdhaval97 <sdhaval97@gmail.com>
2025-09-28 18:19:59 +0530

    started learning lists

---
+++ Lists/lists.py
@@ -0,0 +1,9 @@
+# Python Lists practice
+
+# Accessing/Traversing
+shoppingList = ['Milk', 'Cheese', 'Butter']
+print(shoppingList[0])
+
+# Traversing
+for i in range(len(shoppingList)):
+    print(shoppingList[i])

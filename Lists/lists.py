1577f023e7cf98f080efc6b1d342d0842363bb1d
sdhaval97 <sdhaval97@gmail.com>
2025-10-01 00:32:54 +0530

    updated list practice

--- Lists/lists.py
+++ Lists/lists.py
@@ -28,3 +28,12 @@ def linear_search(list, target):
     return -1
 
 print(linear_search(my_list, 10))
+
+# list comprehension
+new_list = [3, 4, 5, 6, 7, 8]
+double_list = [item * 2 for item in new_list]
+print(double_list)
+new_double_list = [item * 3 for item in new_list if item > 3]
+print(new_double_list)
+filtered_list = [item if item > 20 else 0 for item in new_double_list]
+print(filtered_list)

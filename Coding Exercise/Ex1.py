ce80bb2d432bffdb3ac95c6ed8c631d069cb5405
sdhaval97 <sdhaval97@gmail.com>
2025-10-18 15:14:00 +0530

    deleted the old files

--- Coding Exercise/Ex1.py
+++
@@ -1,7 +0,0 @@
-# find the missing number in an array
-
-def missing_number(my_list, total_count):
-    sum_of_n = total_count * (total_count + 1)/2
-    sum_of_array = sum(my_list)
-    return sum_of_n - sum_of_array
-print(missing_number([1, 2, 3, 4, 6], 6))

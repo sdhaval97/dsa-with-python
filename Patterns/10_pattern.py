869f4d0d439fb479d1b8c90c1bd6c775bd7f4c2f
sdhaval97 <sdhaval97@gmail.com>
2025-10-21 11:26:36 +0530

    created a half diamond pattern

---
+++ Patterns/10_pattern.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the row size: "))
+
+for i in range(1, n+1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+for i in range(n-1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+    

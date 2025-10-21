0702d4abf188b812dd438d6e38841479b976af60
sdhaval97 <sdhaval97@gmail.com>
2025-10-21 23:49:54 +0530

    updated the palindrome code

--- Basic Math/2_reverse_number.py
+++ Basic Math/2_reverse_number.py
@@ -1,18 +1,14 @@
-# reversing a number
-n = int(input("Enter a number you want to reverse: "))
-rev_num = 0
-sign = -1 if n < 0 else 1
-n = abs(n)
-while n > 0:
-    last_digit = n%10
-    rev_num = rev_num * 10 + last_digit
-    n //= 10
-rev_num *= sign
+def is_palindrome(n):
+    if n < 0:
+        return False
+    original = n
+    reverse = 0
+    while n > 0:
+        last_digit = n%10
+        reverse = reverse * 10 + last_digit
+        n //= 10
+    return original == reverse
 
-# Check for 32-bit signed integer overflow
-if rev_num < -2**31 or rev_num > 2**31 - 1:
-    rev_num = 0
-
-print(f"The reversed number is {rev_num}")
-
+        
+print(is_palindrome(121))
     

8eaec42e65520c2708eec73b1f4f28c732a0771c
sdhaval97 <sdhaval97@gmail.com>
2025-10-21 18:48:17 +0530

    12th patter from dsa sheet

---
+++ Patterns/12_pattern.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end="")
+    for j in range(n-i):
+        print(" ", end=" ")
+    for j in range(i, 0, -1):
+        print(j, end="")
+    print()
+    

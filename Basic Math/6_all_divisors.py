ede423cba3232bfa001d4b9ee560193b2e7f0116
sdhaval97 <sdhaval97@gmail.com>
2025-10-23 13:39:22 +0530

    added optimal code to print all divisors

--- Basic Math/6_all_divisors.py
+++ Basic Math/6_all_divisors.py
@@ -8,4 +8,16 @@ def allDivisors(n):
 
 print(allDivisors(4))
 
-            
+# optimal solution
+import math
+def isPrimeOptimal(n):
+    divisors = []
+    for i in range(1, int(math.sqrt(n))+1):
+        if n%i == 0:
+            divisors.append(i)
+            if (n/i != i):
+                divisors.append(int(n/i))
+    
+    return divisors
+
+print(isPrimeOptimal(36))   

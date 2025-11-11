e5c029a773aa1f3ffb02d207a903e0c5b694c775
sdhaval97 <sdhaval97@gmail.com>
2025-11-12 00:23:57 +0530

    node implementation code added

---
+++ CodeCademy/nodes.py
@@ -0,0 +1,15 @@
+# Implementing the good old Node, the cell in the world of data structures
+
+# Creating a class node and creating the constructor
+class Node:
+    def __init__(self, value, link_node=None):
+        self.value = value
+        self.link_node = link_node
+    
+    # Creating getters for link node and value
+    def get_value(self):
+        return self.value
+    
+    def get_link_node(self):
+        return self.link_node
+    

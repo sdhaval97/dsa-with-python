5abadf8fa3c63e2e3edb578b9ff57b42d5110338
sdhaval97 <sdhaval97@gmail.com>
2026-01-09 00:30:46 +0530

    square and rectangle pattern generator

---
+++ New/patterns/pattern_one.py
@@ -0,0 +1,14 @@
+# printing a square
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+        
+# printing a rectangle
+k = int(input("Enter the number of rows: "))
+l = int(input("Enter the number of rows: "))
+for i in range(k):
+    for j in range(l):
+        print("*", end=" ")
+    print()

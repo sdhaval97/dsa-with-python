876203d9e01c76c3229c793c990496b839c48908
Dhaval Shah <sdhaval97@gmail.com>
2026-04-08 14:27:28 +0530

    array practice for the old time sake

---
+++ Arrays.py
@@ -0,0 +1,14 @@
+from array import *
+
+# Practice
+
+# 1. Create an array and traverse
+
+myArray = array('i', [1, 2, 3, 4, 5])
+
+for i in myArray:
+    print(i)
+
+# 2. Access individual elements through indexes
+
+print(myArray[0])
